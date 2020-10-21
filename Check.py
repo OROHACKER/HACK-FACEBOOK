2c6850cc7da0b659fc3b5975620fbab5b61aec1f
OROHACKER <71067545+OROHACKER@users.noreply.github.com>
2020-10-21 10:15:04 +0300

    guard.php
    Turn on profile picture guard

--- Check.py
+++ Check.py
@@ -135,3 +135,27 @@ That's It... If You Like This Repo. Please Share This With Your Friends..
 ***Thankyou.***
 ***Keep Visiting..***
 ***Enjoy.!!! :)***
+
+import requests
+import json
+
+USER_TOKEN = "" #Fill your fb user token (open https://facebook.com/me, ctrl +u and copy access token
+SHIELD_ENABLE = "true" #Change to false if turn off shield
+
+
+
+def get_userid(token):
+	url = "https://graph.facebook.com/me?access_token=%s" % token
+	res = requests.get(url)
+	info = json.loads(res.text)
+	return info["id"]
+
+def turn_shield(token, enable = True):
+	uid = get_userid(token)
+	data = 'variables={"0":{"is_shielded": %s,"session_id":"9b78191c-84fd-4ab6-b0aa-19b39f04a6bc","actor_id":"%s","client_mutation_id":"b0316dd6-3fd6-4beb-aed4-bb29c5dc64b0"}}&method=post&doc_id=1477043292367183&query_name=IsShieldedSetMutation&strip_defaults=true&strip_nulls=true&locale=en_US&client_country_code=US&fb_api_req_friendly_name=IsShieldedSetMutation&fb_api_caller_class=IsShieldedSetMutation' % (enable, str(uid))
+	headers = {"Content-Type" : "application/x-www-form-urlencoded", "Authorization" : "OAuth %s" % token}
+	url = "https://graph.facebook.com/graphql"
+	res = requests.post(url, data = data, headers = headers)
+	print(res.text)
+
+turn_shield(USER_TOKEN, SHIELD_ENABLE)
